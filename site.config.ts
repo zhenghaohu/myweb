48b8b60990d496569281f6370fa25851d1d5aa35
Zhenghao Hu <85891238+zhenghaohu@users.noreply.github.com>
2025-06-07 21:54:36 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '20b52f1a8faa80e78935c504d2924bac',
+  rootNotionPageId: '20b52f1a8faa8080bc60e30d3df07e46',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Zhenghao Hu',
-  domain: 'https://energetic-mass-c95.notion.site/Zhenghao-Hu-20b52f1a8faa80e78935c504d2924bac',
+  domain: 'https://energetic-mass-c95.notion.site/Zhenghao-Hu-20b52f1a8faa8080bc60e30d3df07e46',
   author: 'Zhenghao Hu',
 
   // open graph metadata (optional)
